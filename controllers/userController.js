be547426ffbd13bd78795817b02bac1518aeaea3
StyleLinz_Man <cwchuang1997@gmail.com>
2021-06-13 11:29:00 +0800

    feat: confirm user password and unique email #20210613004

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,14 +6,26 @@ const userController = {
     return res.render('signup')
   },
   signUp: async (req, res) => {
+    const { name, email, password, passwordCheck } = req.body
+    if (password !== passwordCheck) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('/signup')
+    }
     try {
-      const { name, email, password, passwordCheck } = req.body
+      const user = await User.findOne({
+        where: { email }
+      })
+      if (user) {
+        req.flash('error_messages', '信箱重複！')
+        return res.redirect('/signup')
+      }
       const hash = await bcrypt.hash(password, 10)
       await User.create({
         name,
         email,
         password: hash
       })
+      req.flash('success_messages', '帳號註冊成功！')
       return res.redirect('/signin')
     } catch (error) {
       console.error(error)
