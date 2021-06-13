e7da221f1f3596d34401a1fc4adb344bb5057622
StyleLinz_Man <cwchuang1997@gmail.com>
2021-06-13 10:29:16 +0800

    feat: install express-session & connect-flash #20210613003

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 const app = express()
 const port = 3000
 
@@ -9,6 +11,18 @@ app.engine('hbs', exphbs({
 }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Express is listening at http://localhost:${port}`)
