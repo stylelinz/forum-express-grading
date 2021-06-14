9bf9aa4f2eede9c1f538053753a6a2d8a1e2b833
StyleLinz_Man <cwchuang1997@gmail.com>
2021-06-14 22:06:58 +0800

    feat:add users seed data #20210614012

---
+++ seeders/20210614134028-users-seed-file.js
@@ -0,0 +1,37 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [
+      {
+        email: 'root@example.com',
+        password: bcrypt.hashSync('12345678', 10),
+        isAdmin: true,
+        name: 'root',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'user1@example.com',
+        password: bcrypt.hashSync('12345678', 10),
+        isAdmin: false,
+        name: 'user1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'user2@example.com',
+        password: bcrypt.hashSync('12345678', 10),
+        isAdmin: false,
+        name: 'user2',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+}
