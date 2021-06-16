77187992fc53e391a070ae1522763581379fb800
stylelinz <cwchuang1997@gmail.com>
2021-06-16 10:54:30 +0800

    fix: flash messages typo config/passport.js #20210616002

--- config/passport.js
+++ config/passport.js
@@ -20,10 +20,10 @@ passport.use(new LocalStrategy(
         where: { email }
       })
       if (!user) {
-        return done(null, false, req.flash('error_massage', '帳號或密碼輸入錯誤'))
+        return done(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤'))
       }
       if (!bcrypt.compareSync(password, user.password)) {
-        return done(null, false, req.flash('error_massage', '帳號或密碼輸入錯誤'))
+        return done(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤'))
       }
       return done(null, user)
     } catch (error) {
